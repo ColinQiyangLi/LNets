2f085d4873d9061eb5c8914ecc69d371768038d4
Cem Anil <cem.anil@mail.utoronto.ca>
2019-07-29 16:28:09 -0400

    wip image saving code.

--- lnets/tasks/gan/mains/save_gan_samples.py
+++ lnets/tasks/gan/mains/save_gan_samples.py
@@ -1,9 +1,44 @@
+import numpy as np
 
 from lnets.utils.config import process_config
-from lnets.tasks.dualnets.distrib.load_distrib import load_distrib
+from lnets.tasks.dualnets.distrib.gan_sampler import GANSampler
+
+
+def collect_images(sampler, num_imgs, im_size, num_channels, sample_size):
+    # Collect images.
+    sampled_images = np.zeros((num_imgs, im_size, im_size, num_channels))
+
+    count = 0
+    while count < num_imgs:
+        curr_imgs = sampler(cfg.distrib1.sample_size)
+        sampled_images[count:count + sample_size] = curr_imgs
+
+        count += sample_size
+
+    # Add the last bit.
+    last_samples = gan_sampler(cfg.distrib1.sample_size)
+    sampled_images[count:] = last_samples[num_imgs - count]
+
+    return sampled_images
+
+
+def save_images(imgs):
+    for i in range(imgs.shape[0]):
+        import pdb; pdb.set_trace()
 
 
 if __name__ == "__config__":
-    # Load the gan loader. 
-    
+    # Parse the config.
+    cfg = process_config()
 
+    # Quick checks.
+    assert cfg.distrib1.generate_type == "generated"
+
+    # Load the gan loader. 
+    gan_sampler = GANSampler(cfg.distrib1)
+
+    # Collect images.
+    images = collect_images(gan_sampler, cfg.num_imgs, cfg.im_size, cfg.num_channels, cfg.distrib1.sample_size)
+
+    # Save the images.
+    save_images(images)
