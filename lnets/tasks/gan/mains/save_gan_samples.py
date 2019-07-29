b61a16dd6cd10c5753795890aae68a70f34972dc
Cem Anil <cem.anil@mail.utoronto.ca>
2019-07-29 16:01:41 -0400

    Save gan images.

---
+++ lnets/tasks/gan/mains/save_gan_samples.py
@@ -0,0 +1,9 @@
+
+from lnets.utils.config import process_config
+from lnets.tasks.dualnets.distrib.load_distrib import load_distrib
+
+
+if __name__ == "__config__":
+    # Load the gan loader. 
+    
+
